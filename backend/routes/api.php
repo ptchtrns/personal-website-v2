1303637697840264cefe915664184b3ebdfc5eab
ptchtrns <ptchtrns@gmail.com>
2025-01-15 20:07:02 +0200

    set up the project with nextjs & laravel

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+Route::get('/users', function () {
+  return UserResource::collection(User::all());
+});
